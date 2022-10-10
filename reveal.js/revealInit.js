4f26ee51858c4e4356b81639deecdb91eddad313
kenshi244 <maanukyaan@gmail.com>
2022-10-10 21:21:18 +0300

    First commit

---
+++ reveal.js/revealInit.js
@@ -0,0 +1,15 @@
+Reveal.initialize({
+        // Parallax background image
+        parallaxBackgroundImage:
+          "https://consultive.ch/wp-content/uploads/2016/08/consultive-eaccounting-1920x1080.jpg",
+
+        // Parallax background size
+        parallaxBackgroundSize: "1920px 1080px", // CSS syntax, e.g. "2100px 900px" - currently only pixels are supported (don't use % or auto)
+
+        // Number of pixels to move the parallax background per slide
+        // - Calculated automatically unless specified
+        // - Set to 0 to disable movement along an axis
+        parallaxBackgroundHorizontal: 200,
+        parallaxBackgroundVertical: 50,
+        parallaxBackgroundRepeat: "no-repeat",
+      });
